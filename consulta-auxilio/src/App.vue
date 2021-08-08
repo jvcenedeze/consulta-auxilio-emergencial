77f5bcbf99f8dddf537d9699769fa2e39f815060
poiozex <jvcenedeze@hotmail.com>
2021-08-08 20:13:49 -0300

    added axios and started coding Search.vue

--- consulta-auxilio/src/App.vue
+++ consulta-auxilio/src/App.vue
@@ -1,32 +1,31 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-12 col-md-8">
+          <router-link to="/buscar-auxilio">Buscar Auxílio</router-link>
+          <router-link to="/buscar-cep">Buscar CEP</router-link>
+          <router-view />
+        </div>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  height: 100vh;
+  background-color: #9e04f1;
 }
 </style>
---
+++ consulta-auxilio/src/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="input-group">
+            <span
+              class="input-group-text"
+              id="basic-addon1"
+            >
+              <i class="fas fa-map-marked-alt fa-2x"></i>
+            </span>
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Digite o CEP"
+              aria-describedby="basic-addon1"
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
